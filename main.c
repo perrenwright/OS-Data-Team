5d99e4a592b3694d9fa87ed58cba0cca4562763a
Perren Wright <pwright2410@gmail.com>
2021-04-07 17:15:03 -0500

    updated libraries

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
-#include <stdio.h>
+/* This example includes the appropriate error checking
+ * that is not covered in the text.
+ *
+ * Note that OS X does not support unnamed sempahores
+ * so this program will not work on those systems.
+ *
+ * Operating System Concepts - Ninth Edition
+ * John Wiley & Sons - 2013.
+ */
+
 #include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <semaphore.h>
 
 int main(){
     
